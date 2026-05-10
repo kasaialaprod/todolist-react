9cb0b43464fd529991b14dc48bd8bf272b5c1449
Kasai <wanzrael@gmail.com>
2026-05-10 22:55:46 +0200

    manifest and serviceworkerv2

---
+++ public/service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "mon-app-v1";
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./icons/icon.png",
+  "./icons/icon.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
